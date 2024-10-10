f32781516da0c7ff8eea2e840415b3b7a8361ca2
BRANYA43 <tbd2504@gmai.com>
2024-10-10 14:29:47 +0300

    Remove setters for x and y of Vector

--- cli_chess/objects/vector.py
+++ cli_chess/objects/vector.py
@@ -1,27 +1,17 @@
 class Vector:
     def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
+        if not isinstance(x, int) or not isinstance(y, int):
+            raise TypeError('x and y must be integer.')
+        self._x = x
+        self._y = y
 
     @property
     def x(self) -> int:
         return self._x
 
-    @x.setter
-    def x(self, value: int):
-        if not isinstance(value, int):
-            raise TypeError(f'Expected integer for x, but got {type(value).__name__}.')
-        self._x = value
-
     @property
     def y(self) -> int:
         return self._y
-
-    @y.setter
-    def y(self, value: int):
-        if not isinstance(value, int):
-            raise TypeError(f'Expected integer for y, but got {type(value).__name__}.')
-        self._y = value
 
     def __iter__(self):
         return iter((self._x, self._y))
--- cli_chess/tests/test_vector.py
+++ cli_chess/tests/test_vector.py
@@ -11,32 +11,12 @@ class TestVector:
 
     def test_creating_vector_raises_error_if_x_or_y_dont_have_int_type(self):
         # invalid "x" type
-        with raises(TypeError, match='Expected integer for x, but got str.'):
+        with raises(TypeError, match='x and y must be integer.'):
             Vector('1', 0)
 
         # invalid "y: type
-        with raises(TypeError, match='Expected integer for y, but got str.'):
+        with raises(TypeError, match='x and y must be integer.'):
             Vector(0, '2')
-
-    def test_setting_x_raises_error_if_value_isnt_int_type(self):
-        vector = Vector(1, 0)
-
-        assert vector.x == 1
-
-        with raises(TypeError, match='Expected integer for x, but got str.'):
-            vector.x = '3'
-
-        assert vector.x == 1
-
-    def test_setting_y_raises_error_if_value_isnt_int_type(self):
-        vector = Vector(1, 0)
-
-        assert vector.y == 0
-
-        with raises(TypeError, match='Expected integer for y, but got str.'):
-            vector.y = '4'
-
-        assert vector.y == 0
 
     def test_vector_is_iterable(self):
         x, y = Vector(100, 200)
