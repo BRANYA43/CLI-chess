f33dc561bf9db1ab207d3ea9c33fb7bfa8c17c66
BRANYA43 <tbd2504@gmai.com>
2024-10-10 12:58:19 +0300

    Create Direction

--- cli_chess/objects/enums.py
+++ cli_chess/objects/enums.py
@@ -1,5 +1,7 @@
 from enum import IntEnum
 
+from objects.vector import Vector
+
 
 class Color(IntEnum):
     WHITE = 0
@@ -8,3 +10,60 @@ class Color(IntEnum):
     @property
     def opposite_color(self) -> 'Color':
         return Color.BLACK if self == Color.WHITE else Color.WHITE
+
+
+class Direction(IntEnum):
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
+    UP_LEFT = 4
+    UP_RIGHT = 5
+    DOWN_LEFT = 6
+    DOWN_RIGHT = 7
+
+    @classmethod
+    def get_direct_directions(cls) -> tuple['Direction', 'Direction', 'Direction', 'Direction']:
+        return Direction.UP, Direction.DOWN, Direction.LEFT, Direction.RIGHT
+
+    @classmethod
+    def get_diagonal_directions(cls) -> tuple['Direction', 'Direction', 'Direction', 'Direction']:
+        return Direction.UP_LEFT, Direction.UP_RIGHT, Direction.DOWN_LEFT, Direction.DOWN_RIGHT
+
+    @classmethod
+    def get_direction(cls, vector: Vector) -> 'Direction':
+        x, y = vector
+        if x == 0:
+            if y > 0:
+                return Direction.DOWN
+            elif y < 0:
+                return Direction.UP
+        elif x > 0:
+            if y == 0:
+                return Direction.RIGHT
+            elif y > 0:
+                return Direction.DOWN_RIGHT
+            elif y < 0:
+                return Direction.UP_RIGHT
+        elif x < 0:
+            if y == 0:
+                return Direction.LEFT
+            elif y > 0:
+                return Direction.DOWN_LEFT
+            elif y < 0:
+                return Direction.UP_LEFT
+        raise ValueError('Impossible determine a direction for x:0 y:0.')
+
+    @property
+    def vector(self) -> Vector:
+        vectors = {
+            Direction.UP: Vector(0, -1),
+            Direction.DOWN: Vector(0, 1),
+            Direction.LEFT: Vector(-1, 0),
+            Direction.RIGHT: Vector(1, 0),
+            Direction.UP_LEFT: Vector(-1, -1),
+            Direction.UP_RIGHT: Vector(1, -1),
+            Direction.DOWN_LEFT: Vector(-1, 1),
+            Direction.DOWN_RIGHT: Vector(1, 1),
+        }
+        return vectors[self]
--- cli_chess/tests/test_enums.py
+++ cli_chess/tests/test_enums.py
@@ -1,7 +1,56 @@
-from objects.enums import Color
+from pytest import raises
+
+from objects.enums import Color, Direction
+from objects.vector import Vector
 
 
 class TestColorEnum:
     def test_enum_member_returns_opposite_color(self):
         assert Color.WHITE.opposite_color == Color.BLACK
         assert Color.BLACK.opposite_color == Color.WHITE
+
+
+class TestDirectionEnum:
+    def test_getting_direct_directions(self):
+        assert Direction.get_direct_directions() == (Direction.UP, Direction.DOWN, Direction.LEFT, Direction.RIGHT)
+
+    def test_getting_diagonal_directions(self):
+        assert Direction.get_diagonal_directions() == (
+            Direction.UP_LEFT,
+            Direction.UP_RIGHT,
+            Direction.DOWN_LEFT,
+            Direction.DOWN_RIGHT,
+        )
+
+    def test_getting_direction_by_vector(self):
+        directions_vectors = {
+            Direction.UP: Vector(0, -1),
+            Direction.DOWN: Vector(0, 1),
+            Direction.LEFT: Vector(-1, 0),
+            Direction.RIGHT: Vector(1, 0),
+            Direction.UP_LEFT: Vector(-1, -1),
+            Direction.UP_RIGHT: Vector(1, -1),
+            Direction.DOWN_LEFT: Vector(-1, 1),
+            Direction.DOWN_RIGHT: Vector(1, 1),
+        }
+        for direction, vector in directions_vectors.items():
+            assert Direction.get_direction(vector) == direction
+
+    def test_getting_direction_by_vector_raises_error_for_vector_x0_y0(self):
+        vector = Vector(0, 0)
+        with raises(ValueError, match='Impossible determine a direction for x:0 y:0.'):
+            Direction.get_direction(vector)
+
+    def test_vector_property_of_direction(self):
+        directions_vectors = {
+            Direction.UP: Vector(0, -1),
+            Direction.DOWN: Vector(0, 1),
+            Direction.LEFT: Vector(-1, 0),
+            Direction.RIGHT: Vector(1, 0),
+            Direction.UP_LEFT: Vector(-1, -1),
+            Direction.UP_RIGHT: Vector(1, -1),
+            Direction.DOWN_LEFT: Vector(-1, 1),
+            Direction.DOWN_RIGHT: Vector(1, 1),
+        }
+        for direction, vector in directions_vectors.items():
+            assert direction.vector == vector
